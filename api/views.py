7b3ecd59f20e7e600b44efd8f7ff8474e4111333
GlavMech <belbmk20@gmail.com>
2024-08-16 09:49:15 +0500

    create views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import BasePermission, IsAuthenticated
 
-# Create your views here.
+from api.models import ApiUser, Warehouse, Product, Shipment
+from api.serializers import ClientSerializer, WarehouseSerializer, ProductSerializer, ShipmentSerializer
+
+
+class IsSeller(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.group == 'seller'
+
+class IsBuyer(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.group == 'buyer'
+
+
+class ClientModelViewSet(viewsets.ModelViewSet):
+    queryset = ApiUser.objects.all()
+    http_method_names = ['post', 'get']
+    serializer_class = ClientSerializer
+
+    authentication_classes = []
+    permission_classes = []
+
+
+class WarehouseModelViewSet(viewsets.ModelViewSet):
+    queryset = Warehouse.objects.all()
+    serializer_class = WarehouseSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsSeller()]
+        return [IsAuthenticated()]
+
+
+class ProductModelViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsSeller()]
+        return [IsAuthenticated()]
+
+class ShipmentModelViewSet(viewsets.ModelViewSet):
+    queryset = Shipment.objects.all()
+    serializer_class = ShipmentSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'create']:
+            return [IsAuthenticated()]
+        return [IsAuthenticated()]
