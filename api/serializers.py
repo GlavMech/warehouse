451a1a416f7d4514fddde788d2840405d560ce93
GlavMech <165685784+GlavMech@users.noreply.github.com>
2025-08-20 22:05:00 +0500

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,54 +1,38 @@
 from rest_framework import serializers, validators
 from api.models import ApiUser, Warehouse, Product, Shipment
 
-
-class ClientSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=128, validators=[
-     validators.UniqueValidator(ApiUser.objects.all())
-    ])
-    email = serializers.EmailField(validators=[
-     validators.UniqueValidator(ApiUser.objects.all())
-    ])
-    password = serializers.CharField(min_length=6, max_length=128, write_only=True)
-    group = serializers.ChoiceField(choices=ApiUser.ROLE_CHOICES)
+class ClientSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
 
     class Meta:
         model = ApiUser
-        fields = ['username', 'email', 'password', 'group']
-        extra_kwargs = {'password': {'write_only': True}}
-
+        fields = ['id', 'username', 'email', 'password', 'group']
 
     def create(self, validated_data):
-        client = ApiUser.objects.create(
-            email=validated_data["email"],
-            username=validated_data["username"],
-            group=validated_data['group']
-        )
-
-        client.set_password(validated_data["password"])
-        client.save(update_fields=["password"])
-        return client
+        password = validated_data.pop('password')
+        user = ApiUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
     def update(self, instance, validated_data):
-        if email := validated_data.get("email"):
-            instance.email = email
-            instance.save(update_field=["email"])
-        if password := validated_data.get("password"):
+        if password := validated_data.get('password'):
             instance.set_password(password)
-            instance.save(update_field=["password"])
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.group = validated_data.get('group', instance.group)
+        instance.save()
         return instance
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = "__all__"
-        extra_kwargs = {"id": {"read_only": True}}
+        fields = '__all__'
 
 class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Warehouse
-        fields = "__all__"
-        extra_kwargs = {"id": {"read_only": True}}
+        fields = '__all__'
 
 class ShipmentSerializer(serializers.ModelSerializer):
     product = serializers.PrimaryKeyRelatedField(
@@ -58,9 +42,8 @@ class ShipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shipment
         fields = '__all__'
-        extra_kwargs = {"id": {"read_only": True}}
 
     def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['product'] = ProductSerializer(instance.product).data
-        return representation
+        rep = super().to_representation(instance)
+        rep['product'] = ProductSerializer(instance.product).data
+        return rep
