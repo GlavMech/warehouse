3353b3ead06494db9e47f06661e36482ee8cc74f
GlavMech <165685784+GlavMech@users.noreply.github.com>
2025-08-21 20:21:54 +0500

    Update settings.py

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -149,5 +149,8 @@ REST_FRAMEWORK = {
 
 import os
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "api" / "static",  # <-- твоя папка api/static
+]
